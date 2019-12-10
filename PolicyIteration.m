a88eaa21e369f7eed81e42f617ac4d3af64b3083
Nicola Küng <32466285+nkueng@users.noreply.github.com>
2019-12-10 17:13:38 +0100

    working implementation of policy iteration

--- PolicyIteration.m
+++ PolicyIteration.m
@@ -32,30 +32,53 @@ function [ J_opt, u_opt_ind ] = PolicyIteration( P, G )
 
 global K HOVER
 
-% initialize with proper policy (taken from value iteration) 
-% TODO: try HOVER
-% [~, u_opt_init] = ValueIteration(P, G);
-u_opt_init = 5 * ones(476, 1);
-
-% policy evaluation: solve system of linear equations
-P_opt = zeros(K, K);
-% TODO: find better way to do this
-for i = 1 : K
-    P_opt(i, :) = P(i, :, u_opt_init(i));
-end
-G_opt = diag(G(:,u_opt_init));
-
-J_opt = (eye(size(P_opt)) - P_opt)\G_opt;
-
-% policy improvement: obtain new stationary policy
-
-
-
 %% Handle terminal state
 % Do yo need to do something with the teminal state before starting policy
 % iteration?
 global TERMINAL_STATE_INDEX
 % IMPORTANT: You can use the global variable TERMINAL_STATE_INDEX computed
 % in the ComputeTerminalStateIndex.m file (see main.m)
+
+J_opt = zeros(K, 1);
+
+% initialize with proper policy
+% using optimal policy from value iteration
+% [~, u_opt_ind] = ValueIteration(P, G);
+% commanding HOVER at all states
+u_opt_ind = 5 * ones(476, 1);
+
+while true
+    
+    J_opt_prev = J_opt;
+    
+    % policy evaluation: solve system of linear equations
+    % P_opt and G_opt correspond to P and G with the current optimal policy
+    % without the terminal state
+    P_opt = zeros(K, K);
+    % TODO: find better way to do this
+    for i = 1 : K
+        if i == TERMINAL_STATE_INDEX
+            P_opt(i, :) = zeros(1, K);
+        else
+            P_opt(i, :) = P(i, :, u_opt_ind(i));
+        end
+    end
+    G_opt = diag(G(:,u_opt_ind));
+
+    % solve for J: (I - P) must be invertible!
+    J_opt = (eye(size(P_opt)) - P_opt)\G_opt;
+    
+    % exit condition: iterate until cost-to-go is optimal
+    if J_opt_prev == J_opt
+        break;
+    end
+
+    % policy improvement: obtain new stationary policy (same as VI)
+    for i = 1 : K
+        [~, u_opt_ind(i)] = min(G(i, :)' + squeeze(P(i, :, :))'*J_opt);
+    end
+end
+
+J_opt(TERMINAL_STATE_INDEX) = 0;
 
 end
